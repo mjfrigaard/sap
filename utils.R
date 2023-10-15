f61560e73bdaf5ef30c8a708a33acfc45af84a1f
Martin Frigaard <mjfrigaard@pm.me>
2023-10-15 11:47:24 -0700

    file cleanup

--- utils.R
+++
@@ -1,8 +0,0 @@
-scatter_plot <- function(df, x_var, y_var, col_var, alpha_var, size_var) {
-    ggplot2::ggplot(data = df,
-      ggplot2::aes(x = .data[[x_var]],
-          y = .data[[y_var]],
-          color = .data[[col_var]])) +
-      ggplot2::geom_point(alpha = alpha_var, size = size_var)
-
-}

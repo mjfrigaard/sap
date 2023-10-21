d114b9600ab2eef61b7b8233ef765d9f8e94017c
Martin Frigaard <mjfrigaard@pm.me>
2023-10-20 21:10:10 -0700

    updates to dev function and app.R

--- R/ggp2_movies_app.R
+++ R/ggp2_movies_app.R
@@ -2,34 +2,37 @@
 #'
 #' Wrapper function for `shinyAppDir()`
 #' 
-#' @param test logical, run in `test.mode`? Defaults to `TRUE`.
+#' @param test logical, run in `test.mode`? Defaults to `FALSE`.
+#' @param run where to launch app: 
+#'  * `p` = launch in viewer pane
+#'  * `b` = launch in external browser  
+#'  * `w` = launch in window
+#'  * `NULL` = `getOption(x = 'shiny.launch.browser')`
 #' 
 #' @return shiny app
 #' 
 #'
 #' @export
-ggp2_movies_app <- function(test = TRUE, run = "w") {
+ggp2_movies_app <- function(test = FALSE, run = "w") {
+  
   if (interactive()) {
-    display_type(run = run)
-
-    if (isTRUE(test)) {
-      shinyAppDir(
-        appDir = system.file("dev",
-          package = "moviesApp"
-        ),
-        options(test.mode = TRUE)
-      )
-    } else {
-      shinyAppDir(
-        appDir = system.file("dev",
-          package = "moviesApp"
-        ),
-        options(test.mode = FALSE)
-      )
-    }
+      display_type(run = run)
+      if (test) {
+        shinyAppDir(
+          appDir = system.file("dev", 
+                               package = "moviesApp"),
+          options(test.mode = TRUE)
+        )
+      } else {
+        shinyAppDir(
+          appDir = system.file("dev", 
+                               package = "moviesApp")
+        )
+      }
   } else {
-    shinyAppDir(appDir = system.file("dev",
-      package = "moviesApp"
-    ))
+      shinyAppDir(
+        appDir = system.file("dev", 
+                             package = "moviesApp"))
   }
+  
 }
--- app.R
+++ app.R
@@ -20,8 +20,5 @@ withr::with_options(new = list(shiny.autoload.r = FALSE), code = {
     pkgload::load_all()
   }
   # create shiny object ----
-  shinyApp(
-    ui = movies_ui,
-    server = movies_server
-  )
+  moviesApp::ggp2_movies_app(run = 'p')
 })

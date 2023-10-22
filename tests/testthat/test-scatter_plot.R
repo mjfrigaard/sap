243078299c8c17e1abe8c3beba3450f8a707cb06
Martin Frigaard <mjfrigaard@pm.me>
2023-10-22 16:15:26 -0700

    update scatter_plot() test

--- tests/testthat/test-scatter_plot.R
+++ tests/testthat/test-scatter_plot.R
@@ -16,7 +16,9 @@ testthat::describe(
               When the scatter plot renders,
               Then the points on the x axis should represent 'Ratings'
               And the points on the y axis should represent 'Length'
-              And the points should be colored by 'MPAA' rating",
+              And the points should be colored by 'MPAA' rating
+              And the size of the points should be set to '2'
+              And the opacity of the points should be set to '0.5'",
           code = {
             test_logger(start = "T1", msg = "Create ggplot2 object")
             # inputs
@@ -42,10 +44,15 @@ testthat::describe(
               size_var = ggp2_scatter_inputs$size
             )
             expect_true(ggplot2::is.ggplot(app_graph))
+
             test_logger(end = "T1", msg = "Create ggplot2 object")
-          })
-      })
-  })
+          }
+        )
+      }
+    )
+  }
+)
+
 testthat::describe(
   "Feature 1.2: Scatter Plot Configuration in Movie Review Application
     As a user who accesses the movie review application,
@@ -53,14 +60,14 @@ testthat::describe(
     So that I can immediately see a meaningful visualization.",
   code = {
     testthat::it(
-      "Scenario: Displaying the Pre-configured Initial Scatter Plot
-      Given the movie review application is loaded
-      When I view the initial scatter plot
-      Then the x-axis should display the 'IMDB Rating'
-      And the y-axis should display the 'Audience Score'
-      And the points should be colored by 'MPAA Rating'
-      And the size of the points should be set to '2'
-      And the opacity of the points should be set to '0.5'",
+      "Scenario: Create scatter plot
+          Given I have launched the movie review exploration app,
+          When the scatter plot renders,
+          Then the points on the x axis should represent 'Ratings'
+          And the points on the y axis should represent 'Length'
+          And the points should be colored by 'MPAA' rating
+          And the size of the points should be set to '2'
+          And the opacity of the points should be set to '0.5'",
       code = {
         test_logger(start = "T2", msg = "Tests initial x/y/z")
 
@@ -100,56 +107,68 @@ testthat::describe(
         )
 
         test_logger(end = "T2", msg = "Tests initial x/y/z")
-      })
+      }
+    )
+  }
+)
 
-    testthat::it(
-      "Scenario: Change x, y, color values for plotting
-    When I launch the Scatter Plot Data Visualization
-    And I select the variable 'Audience Score' for the x-axis
-    And I select the variable 'IMDB Rating' for the y-axis
-    And I select the variable 'Critics Rating' for the color
-    Then the scatter plot should show 'Audience Score' on the x-axis
-    And the scatter plot should show 'IMDB Rating' on the y-axis
-    And the points on the scatter plot should be colored by 'Critics Rating'
+testthat::describe("
+  Feature: Scatter Plot Points Controlled by Dropdown Menus
+    As a user creating a scatter plot,
+    I want dropdown menus to select continuous variables for the x and y axes a
+    And I want a dropdown menu to select a categorical variable for point coloration,
+    So that I can effectively visualize the relationships and categories within the data.", 
+  
+  code = {
+  testthat::it(
+    "Scenario: Change x, y, color values for plotting
+        When I launch the Scatter Plot Data Visualization
+        And I select the variable 'Audience Score' for the x-axis
+        And I select the variable 'IMDB Rating' for the y-axis
+        And I select the variable 'Critics Rating' for the color
+        Then the scatter plot should show 'Audience Score' on the x-axis
+        And the scatter plot should show 'IMDB Rating' on the y-axis
+        And the points on the scatter plot should be colored by 'Critics Rating'
   ",
-      code = {
-        test_logger(start = "T3", msg = "Tests updated x/y/z")
+    code = {
+      test_logger(start = "T3", msg = "Tests updated x/y/z")
 
-        scatter_inputs <- list(
-          x = "audience_score",
-          y = "imdb_rating",
-          z = "critics_rating",
-          alpha = 0.5,
-          size = 2,
-          plot_title = "Enter plot title"
-        )
+      scatter_inputs <- list(
+        x = "audience_score",
+        y = "imdb_rating",
+        z = "critics_rating",
+        alpha = 0.5,
+        size = 2,
+        plot_title = "Enter plot title"
+      )
 
-        vdiffr::expect_doppelganger(
-          title = "Updated x, y, color",
-          fig = scatter_plot(movies,
-            x_var = scatter_inputs$x,
-            y_var = scatter_inputs$y,
-            col_var = scatter_inputs$z,
-            alpha_var = scatter_inputs$alpha,
-            size_var = scatter_inputs$size
+      vdiffr::expect_doppelganger(
+        title = "Updated x, y, color",
+        fig = scatter_plot(movies,
+          x_var = scatter_inputs$x,
+          y_var = scatter_inputs$y,
+          col_var = scatter_inputs$z,
+          alpha_var = scatter_inputs$alpha,
+          size_var = scatter_inputs$size
+        ) +
+          ggplot2::labs(
+            title = scatter_inputs$plot_title,
+            x = stringr::str_replace_all(
+              tools::toTitleCase(
+                scatter_inputs$x
+              ), "_", " "
+            ),
+            y = stringr::str_replace_all(
+              tools::toTitleCase(
+                scatter_inputs$y
+              ), "_", " "
+            )
           ) +
-            ggplot2::labs(
-              title = scatter_inputs$plot_title,
-              x = stringr::str_replace_all(
-                tools::toTitleCase(
-                  scatter_inputs$x
-                ), "_", " "
-              ),
-              y = stringr::str_replace_all(
-                tools::toTitleCase(
-                  scatter_inputs$y
-                ), "_", " "
-              )
-            ) +
-            ggplot2::theme_minimal() +
-            ggplot2::theme(legend.position = "bottom")
-        )
+          ggplot2::theme_minimal() +
+          ggplot2::theme(legend.position = "bottom")
+      )
 
-        test_logger(end = "T3", msg = "Tests updated x/y/z")
-      })
-  })
+      test_logger(end = "T3", msg = "Tests updated x/y/z")
+    }
+  )
+})

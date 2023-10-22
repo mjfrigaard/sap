788fc291f1f198cc79e3c75e93a3555e4a479415
Martin Frigaard <mjfrigaard@pm.me>
2023-10-22 13:45:46 -0700

    updates BDD test

--- tests/testthat/test-scatter_plot.R
+++ tests/testthat/test-scatter_plot.R
@@ -1,21 +1,22 @@
 testthat::describe(
-    "Feature: Scatter plot data visualization
+  "Feature: Scatter plot data visualization
        As a film data analyst
        I want to explore movie review data from IMDB.com
        So that I can analyze relationships between movie reivew metrics",
   code = {
-  testthat::describe(
-    "Background:
+    testthat::describe(
+      "Background:
        Given I have data with IMDB movie reviews
        And the data contains continuous variables like 'rating'
        And the data contains categorical variables like 'mpaa'",
-    code = {
-      testthat::it(
-        "Scenario: Create scatter plot
-           Given I have launched the movie review exploration app,
-           When I view the scatter plot,
-           Then I should see points representing values for a default
-                set of continuous and categorical columns.", 
+      code = {
+        testthat::it(
+          "Scenario: Create scatter plot
+     Given I have launched the movie review exploration app,
+     When the scatter plot renders,
+     Then the points on the x axis should represent 'Ratings'
+     And the points on the y axis should represent 'Length'
+     And the points should be colored by 'MPAA' rating",
           code = {
             
         })

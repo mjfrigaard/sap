9282ceedc5c2902f4eec02a638fd438893cf3206
Martin Frigaard <mjfrigaard@pm.me>
2023-10-22 13:47:56 -0700

    update tabs

--- tests/testthat/test-scatter_plot.R
+++ tests/testthat/test-scatter_plot.R
@@ -6,17 +6,17 @@ testthat::describe(
   code = {
     testthat::describe(
       "Background:
-       Given I have data with IMDB movie reviews
-       And the data contains continuous variables like 'rating'
-       And the data contains categorical variables like 'mpaa'",
+          Given I have data with IMDB movie reviews
+          And the data contains continuous variables like 'rating'
+          And the data contains categorical variables like 'mpaa'",
       code = {
         testthat::it(
           "Scenario: Create scatter plot
-     Given I have launched the movie review exploration app,
-     When the scatter plot renders,
-     Then the points on the x axis should represent 'Ratings'
-     And the points on the y axis should represent 'Length'
-     And the points should be colored by 'MPAA' rating",
+              Given I have launched the movie review exploration app,
+              When the scatter plot renders,
+              Then the points on the x axis should represent 'Ratings'
+              And the points on the y axis should represent 'Length'
+              And the points should be colored by 'MPAA' rating",
           code = {
             # inputs
             ggp2_scatter_inputs <- list(

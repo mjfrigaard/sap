2d0250934368b28c4672c79721c2d062459a94f7
Martin Frigaard <mjfrigaard@pm.me>
2023-10-22 13:44:51 -0700

    update BDD tests

--- tests/testthat/test-scatter_plot.R
+++ tests/testthat/test-scatter_plot.R
@@ -1,44 +1,46 @@
 testthat::describe(
-    "F1.2-1.4: Dropdown menus (continuous x & y axes, categorical point coloration)
-
+  "Feature: Scatter plot data visualization
        As a film data analyst
        I want to explore movie review data from IMDB.com
-       So that I can analyze relationships between movie reivew metrics
-  
-     Background:
+       So that I can analyze relationships between movie reivew metrics",
+  code = {
+    testthat::describe(
+      "Background:
        Given I have data with IMDB movie reviews
        And the data contains continuous variables like 'rating'
-       And the data contains categorical variables like 'mpaa'", 
-       code = {
-testthat::test_that(
-  "Scenario: Scatter plot initial x, y, color values 
-    When I launched the Scatter Plot Data Visualization
-    And I have a dataset of movie reviews from IMDB 
-    Then the scatter plot should show 'Rating' on the x-axis
-    And the scatter plot should show 'Length' on the y-axis
-    And the points on the scatter plot should be colored by 'MPAA' rating", 
+       And the data contains categorical variables like 'mpaa'",
+      code = {
+        testthat::it(
+          "Scenario: Create scatter plot
+     Given I have launched the movie review exploration app,
+     When the scatter plot renders,
+     Then the points on the x axis should represent 'Ratings'
+     And the points on the y axis should represent 'Length'
+     And the points should be colored by 'MPAA' rating",
           code = {
-    # inputs
-        ggp2_scatter_inputs <- list(
-          x = "rating",
-          y = "length",
-          z = "mpaa",
-          alpha = 0.75,
-          size = 3,
-          plot_title = "Enter plot title"
-        )
-    # data fixture
-    ggp2_movies <- readRDS(test_path("fixtures",
-                            "tidy_ggp2_movies.rds"))
-    
-    app_graph <- scatter_plot(ggp2_movies,
-      x_var = ggp2_scatter_inputs$x,
-      y_var = ggp2_scatter_inputs$y,
-      col_var = ggp2_scatter_inputs$z,
-      alpha_var = ggp2_scatter_inputs$alpha,
-      size_var = ggp2_scatter_inputs$size
-    )
-    expect_true(ggplot2::is.ggplot(app_graph))
-  })
+            # inputs
+            ggp2_scatter_inputs <- list(
+              x = "rating",
+              y = "length",
+              z = "mpaa",
+              alpha = 0.75,
+              size = 3,
+              plot_title = "Enter plot title"
+            )
+            # data fixture
+            ggp2_movies <- readRDS(test_path(
+              "fixtures",
+              "tidy_ggp2_movies.rds"
+            ))
 
+            app_graph <- scatter_plot(ggp2_movies,
+              x_var = ggp2_scatter_inputs$x,
+              y_var = ggp2_scatter_inputs$y,
+              col_var = ggp2_scatter_inputs$z,
+              alpha_var = ggp2_scatter_inputs$alpha,
+              size_var = ggp2_scatter_inputs$size
+            )
+            expect_true(ggplot2::is.ggplot(app_graph))
+        })
+    })
 })

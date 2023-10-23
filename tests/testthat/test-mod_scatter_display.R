484917521780941dd216289286f478a123207e2b
Martin Frigaard <mjfrigaard@pm.me>
2023-10-22 21:57:41 -0700

    format test file

--- tests/testthat/test-mod_scatter_display.R
+++ tests/testthat/test-mod_scatter_display.R
@@ -55,22 +55,22 @@ testthat::describe(
   "Feature F1.3: Scatter plot data visualization (outputs rendering)
      As a film data analyst
      I want to explore continuous and categorical variables in the movie review data
-     So that I can analyze relationships between movie reivew metrics", code = {
-  testthat::it(
-      "Scenario: Change x, y, color values for plotting
-          Given the movie review application is loaded
-          When I choose the variable 'IMDB number of votes' for the x-axis
-          And I choose the variable 'Critics Score' for the y-axis
-          And I choose the variable 'Genre' for the color
-          And I choose the size of the points to be 3
-          And I choose the opacity of the points to be 0.7
-          And I choose the title of the plot to be 'New plot title'
-          Then the scatter plot should show 'IMDB number of votes' on the x-axis
-          And the scatter plot should show 'Critics Score' on the y-axis
-          And the points on the scatter plot should be colored by 'Genre'
-          And the size of the points on the scatter plot should be 3
-          And the opacity of the points on the scatter plot should be 0.7
-          And the title of the plot should be 'New Plot Title'", code = {
+     So that I can analyze relationships between movie reivew metrics
+    
+  Scenario: Change x, y, color values for plotting
+     Given the movie review application is loaded
+     When I choose the variable 'IMDB number of votes' for the x-axis
+     And I choose the variable 'Critics Score' for the y-axis
+     And I choose the variable 'Genre' for the color
+     And I choose the size of the points to be 3
+     And I choose the opacity of the points to be 0.7
+     And I choose the title of the plot to be 'New plot title'", code = {
+  testthat::it("Then the scatter plot should show 'IMDB number of votes' on the x-axis
+                And the scatter plot should show 'Critics Score' on the y-axis
+                And the points on the scatter plot should be colored by 'Genre'
+                And the size of the points on the scatter plot should be 3
+                And the opacity of the points on the scatter plot should be 0.7
+                And the title of the plot should be 'New Plot Title'", code = {
     shiny::testServer(
       app = mod_scatter_display_server,
       args = list(var_inputs =

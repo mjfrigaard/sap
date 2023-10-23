e3f3aa64c7f33f5524d60926ae42da4011042b30
Martin Frigaard <mjfrigaard@pm.me>
2023-10-23 14:15:02 -0700

    add test_logger() to feature-01 test

--- tests/testthat/test-feature-01.R
+++ tests/testthat/test-feature-01.R
@@ -16,7 +16,8 @@ testthat::describe(
                  And the points on the scatter plot should be colored by 'Genre'
                  And the size of the points on the scatter plot should be 3
                  And the opacity of the points on the scatter plot should be 0.7
-                 And the title of the plot should be 'New Plot Title'", {
+                 And the title of the plot should be 'New Plot Title'", code = {
+    test_logger(start = 'feature-01', msg = "Change x/y/z, size, alpha, title")
     app <- AppDriver$new(name = "moviesApp-feature-01", height = 800, width = 1173)
     app$set_inputs(`vars-y` = "imdb_num_votes")
     app$set_inputs(`vars-x` = "critics_score")
@@ -25,6 +26,7 @@ testthat::describe(
     app$set_inputs(`vars-size` = 3)
     app$set_inputs(`vars-plot_title` = "New plot title")
     app$expect_values()
+    test_logger(end = 'feature-01', msg = "Change x/y/z, size, alpha, title")
   })
 })
 

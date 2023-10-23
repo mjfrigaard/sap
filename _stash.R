79a74f1eee5a7f6746cd425e9c57a768e35a17be
Martin Frigaard <mjfrigaard@pm.me>
2023-10-22 20:55:20 -0700

    stash

--- _stash.R
+++ _stash.R
@@ -1,3 +1,227 @@
+# STANDALONE APP FUNCTION -------------------------------------------------------------------------------
+#' Movies app standalone function
+#'
+#' Wrapper function for `shinyApp()`
+#'
+#' @param options arguments to pass to `options()`
+#' @param ... arguments passed to UI
+#' @param run where to launch app:
+#'  * `p` = launch in viewer pane
+#'  * `b` = launch in external browser
+#'  * `w` = launch in window
+#'
+#'
+#' @return shiny app
+#'
+#'
+#' @seealso [mod_var_input_ui()], [mod_var_input_server()], [mod_scatter_display_ui()], [mod_scatter_display_server()]
+#'
+#' @import shiny
+#'
+#' @export
+#'
+movies_app <- function(options = list(), run = "p", ...) {
+  if (interactive()) {
+    display_type(run = run)
+  }
+    shinyApp(
+      ui = movies_ui(),
+      server = movies_server,
+      options = options
+    )
+}
+
+
+# APP.R -----------------------------------------------------------------
+withr::with_options(new = list(shiny.autoload.r = FALSE), code = {
+  if (!interactive()) {
+    sink(stderr(), type = "output")
+    tryCatch(
+      expr = {
+        # load package ----
+        library(moviesApp)
+      },
+      error = function(e) {
+        # load R/ folder ----
+        pkgload::load_all()
+      }
+    )
+  } else {
+    # load R/ folder ----
+    pkgload::load_all()
+  }
+  # create shiny object ----
+    moviesApp::movies_app(
+      options = list(test.mode = TRUE), run = 'p')
+})
+
+# GGPLOT2MOVIES::MOVIES APP FUNCTION ---------------------------------------------------------------------------
+#' Development `ggplot2movies` app standalone function
+#'
+#' Wrapper function for `shinyAppDir()`
+#'
+#' @param options arguments to pass to `options()`
+#' @param run where to launch app:
+#'  * `p` = launch in viewer pane
+#'  * `b` = launch in external browser
+#'  * `w` = launch in window
+#'  * `NULL` = `getOption(x = 'shiny.launch.browser')`
+#'
+#' @return shiny app
+#'
+#'
+#' @export
+ggp2_movies_app <- function(options = list(), run = "w", ...) {
+  if (interactive()) {
+    display_type(run = run)
+  } 
+    shinyAppDir(
+    appDir = system.file("dev",
+      package = "moviesApp"
+    ),
+    options = options
+  )
+}
+
+# IGNORE FILES ---------------------------------------------------------
+# .Rproj.user
+# .Rhistory
+# .RData
+# .Ruserdata
+# .DS_Store
+# moviesApp.Rcheck/
+# moviesApp*.tar.gz
+# moviesApp*.tgz
+
+
+# ^.*\.Rproj$
+# ^\.Rproj\.user$
+# ^moviesApp\.Rcheck$
+# ^moviesApp.*\.tar\.gz$
+# ^moviesApp.*\.tgz$
+
+# SNAPSHOT TESTS -------------------------------------------------------------
+testthat::describe(
+  "Feature 1.2: Scatter Plot Configuration in Movie Review Application
+    As a user who accesses the movie review application,
+    I want the initial scatter plot pre-configured with variables and aesthetics,
+    So that I can immediately see a meaningful visualization.",
+  code = {
+    testthat::it(
+      "Scenario: Create scatter plot
+          Given I have launched the movie review exploration app,
+          When the scatter plot renders,
+          Then the points on the x axis should represent 'Ratings'
+          And the points on the y axis should represent 'Length'
+          And the points should be colored by 'MPAA' rating
+          And the size of the points should be set to '2'
+          And the opacity of the points should be set to '0.5'",
+      code = {
+        test_logger(start = "T2", msg = "Tests initial x/y/z")
+
+        scatter_inputs <- list(
+          x = "imdb_rating",
+          y = "audience_score",
+          z = "mpaa_rating",
+          alpha = 0.5,
+          size = 2,
+          plot_title = "Enter plot title"
+        )
+
+        vdiffr::expect_doppelganger(
+          title = "Initial x y z axes",
+          fig = scatter_plot(movies,
+            x_var = scatter_inputs$x,
+            y_var = scatter_inputs$y,
+            col_var = scatter_inputs$z,
+            alpha_var = scatter_inputs$alpha,
+            size_var = scatter_inputs$size
+          ) +
+            ggplot2::labs(
+              title = scatter_inputs$plot_title,
+              x = stringr::str_replace_all(
+                tools::toTitleCase(
+                  scatter_inputs$x
+                ), "_", " "
+              ),
+              y = stringr::str_replace_all(
+                tools::toTitleCase(
+                  scatter_inputs$y
+                ), "_", " "
+              )
+            ) +
+            ggplot2::theme_minimal() +
+            ggplot2::theme(legend.position = "bottom")
+        )
+
+        test_logger(end = "T2", msg = "Tests initial x/y/z")
+      }
+    )
+  }
+)
+
+testthat::describe("
+  Feature: Scatter Plot Points Controlled by Dropdown Menus
+    As a user creating a scatter plot,
+    I want dropdown menus to select continuous variables for the x and y axes a
+    And I want a dropdown menu to select a categorical variable for point coloration,
+    So that I can effectively visualize the relationships and categories within the data.", 
+  
+  code = {
+  testthat::it(
+    "Scenario: Change x, y, color values for plotting
+        When I launch the Scatter Plot Data Visualization
+        And I select the variable 'Audience Score' for the x-axis
+        And I select the variable 'IMDB Rating' for the y-axis
+        And I select the variable 'Critics Rating' for the color
+        Then the scatter plot should show 'Audience Score' on the x-axis
+        And the scatter plot should show 'IMDB Rating' on the y-axis
+        And the points on the scatter plot should be colored by 'Critics Rating'
+  ",
+    code = {
+      test_logger(start = "T3", msg = "Tests updated x/y/z")
+
+      scatter_inputs <- list(
+        x = "audience_score",
+        y = "imdb_rating",
+        z = "critics_rating",
+        alpha = 0.5,
+        size = 2,
+        plot_title = "Enter plot title"
+      )
+
+      vdiffr::expect_doppelganger(
+        title = "Updated x, y, color",
+        fig = scatter_plot(movies,
+          x_var = scatter_inputs$x,
+          y_var = scatter_inputs$y,
+          col_var = scatter_inputs$z,
+          alpha_var = scatter_inputs$alpha,
+          size_var = scatter_inputs$size
+        ) +
+          ggplot2::labs(
+            title = scatter_inputs$plot_title,
+            x = stringr::str_replace_all(
+              tools::toTitleCase(
+                scatter_inputs$x
+              ), "_", " "
+            ),
+            y = stringr::str_replace_all(
+              tools::toTitleCase(
+                scatter_inputs$y
+              ), "_", " "
+            )
+          ) +
+          ggplot2::theme_minimal() +
+          ggplot2::theme(legend.position = "bottom")
+      )
+
+      test_logger(end = "T3", msg = "Tests updated x/y/z")
+    }
+  )
+})
+
+
 # library(covtracer)
 
 # additional demo packages

9b96fbd20aebef8f85a5f5af02c86e75e7e2df6e
Martin Frigaard <mjfrigaard@pm.me>
2023-10-22 19:15:44 -0700

    new tests for helpers

--- tests/testthat/test-scatter_plot.R
+++ tests/testthat/test-scatter_plot.R
@@ -20,7 +20,7 @@ testthat::describe(
               And the size of the points should be set to '2'
               And the opacity of the points should be set to '0.5'",
           code = {
-            test_logger(start = "T1", msg = "Create ggplot2 object")
+            test_logger(start = "Test 1", msg = "fixtures/tidy_ggp2_movies.rds")
             # inputs
             ggp2_scatter_inputs <- list(
               x = "rating",
@@ -44,11 +44,49 @@ testthat::describe(
               size_var = ggp2_scatter_inputs$size
             )
             expect_true(ggplot2::is.ggplot(app_graph))
+            test_logger(start = "Test 1", msg = "fixtures/tidy_ggp2_movies.rds")
+        })
+    })
+})
 
-            test_logger(end = "T1", msg = "Create ggplot2 object")
-          }
-        )
-      }
-    )
-  }
-)
+testthat::describe(
+  "Feature: Scatter plot data visualization
+       As a film data analyst
+       I want to explore movie review data from IMDB & Rotten Tomatoes
+       So that I can analyze relationships between movie reivew metrics
+    Background:
+          Given I have data with IMDB & Rotten Tomatoes movie reviews
+          And the data contains continuous variables like 'Audience Score'
+          And the data contains categorical variables like 'MPAA Rating'",
+  code = {
+        testthat::it(
+          "Scenario: Create scatter plot
+              Given I have launched the movie review exploration app,
+              When the scatter plot renders,
+              Then the points on the x axis should represent 'IMDB Rating'
+              And the points on the y axis should represent 'Audience Score'
+              And the points should be colored by 'MPAA' rating
+              And the size of the points should be set to '2'
+              And the opacity of the points should be set to '0.5'",
+          code = {
+            test_logger(start = "Test 2", msg = "data/movies.rda")
+            # inputs
+            scatter_inputs <- list(y = 'audience_score', 
+                                   x = 'imdb_rating',
+                                   z = 'mpaa_rating',
+                                   alpha = 0.5,
+                                   size = 2,
+                                   plot_title = 'Enter plot title')
+
+            app_graph <- scatter_plot(movies,
+              x_var = scatter_inputs$x,
+              y_var = scatter_inputs$y,
+              col_var = scatter_inputs$z,
+              alpha_var = scatter_inputs$alpha,
+              size_var = scatter_inputs$size
+            )
+            expect_true(ggplot2::is.ggplot(app_graph))
+
+            test_logger(end = "Test 2", msg = "data/movies.rda")
+        })
+})

db1c4ff001b945b1957a71364f20b335333ac630
Martin Frigaard <mjfrigaard@pm.me>
2023-10-23 20:43:51 -0700

    white space

--- R/movies_server.R
+++ R/movies_server.R
@@ -43,10 +43,8 @@ movies_server <- function(input, output, session) {
   
     observe({
       browser()
-      
       selected_vars <- mod_var_input_server("vars")
-
       mod_scatter_display_server("plot", var_inputs = selected_vars)
-      
     })
+  
 }

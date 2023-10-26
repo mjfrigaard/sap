a7773c6309c0bf7c2b17cbec1e146a1d243f84cb
Martin Frigaard <mjfrigaard@pm.me>
2023-10-26 13:12:50 -0700

    test updates

--- inst/dev/app.R
+++ inst/dev/app.R
@@ -15,7 +15,7 @@ dev_mod_vars_ui <- function(id) {
         "Year" = "year",
         "Length" = "length",
         "Budget" = "budget",
-        "Rating" = "avg_rating",
+        "Rating" = "rating",
         "Votes" = "votes"
       ),
       selected = "budget"
@@ -27,7 +27,7 @@ dev_mod_vars_ui <- function(id) {
         "Year" = "year",
         "Length" = "length",
         "Budget" = "budget",
-        "Rating" = "avg_rating",
+        "Rating" = "rating",
         "Votes" = "votes"
       ),
       selected = "year"
@@ -75,7 +75,7 @@ dev_mod_scatter_ui <- function(id) {
       checkboxInput(
         inputId = ns("missing"),
         label = "Remove missing",
-        value = TRUE
+        value = FALSE
       ),
       plotOutput(outputId = ns("scatterplot"))
     )

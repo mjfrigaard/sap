f8be1f28714b745cc7647387b0b924a504f08df2
Martin Frigaard <mjfrigaard@pm.me>
2023-11-21 22:24:11 -0800

    new tests

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -6,8 +6,28 @@ box::use(
   app/main[...],
 )
 
-test_that("main server works", {
-  testServer(server, {
-    expect_equal(output$message, "Hello!")
-  })
-})
+# test_that("main server works", {
+#     shiny::testServer(inputs$server, expr = {
+#       
+#       # create list of output vals
+#       test_vals <- list(y = "imdb_rating",
+#                         x = "audience_score",
+#                         z = "mpaa_rating",
+#                         alpha = 0.75,
+#                         size = 3,
+#                         plot_title = "Example title")
+# 
+#       # change inputs
+#       session$setInputs(y = "imdb_rating",
+#                         x = "audience_score",
+#                         z = "mpaa_rating",
+#                         alpha = 0.75,
+#                         size = 3,
+#                         plot_title = "Example title")
+# 
+#       testthat::expect_equal(
+#         object = session$returned(),
+#         expected = test_vals
+#       )
+#   })
+# })

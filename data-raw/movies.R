09a4f6459eedc06511081c40548505a3f8aae27a
Martin Frigaard <mjfrigaard@pm.me>
2023-12-08 20:02:48 -0700

    plain text data

--- data-raw/movies.R
+++ data-raw/movies.R
@@ -1,5 +1,4 @@
 ## code to prepare `movies` dataset goes here
-# library(fst)
-movies <- fst::read_fst(path = "inst/extdata/movies.fst")
+load("data/movies.rda")
 # store as plain text file for easier sharing
 write.csv(x = movies, file = "inst/extdata/movies.csv")

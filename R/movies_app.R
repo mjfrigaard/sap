0257cd059709e01abde89709660f6d22d6b6caf5
Martin Frigaard <mjfrigaard@pm.me>
2024-01-09 21:26:49 -0700

    new workflow trigger

--- R/movies_app.R
+++ R/movies_app.R
@@ -19,13 +19,13 @@
 #'
 #' @export
 #'
-movies_app <- function(options = list(), run = "p", ...) {
+movies_app <- function(options = list(),
+  run = "p",
+  ...) {
   if (interactive()) {
     display_type(run = run)
   }
-  shinyApp(
-    ui = movies_ui(...),
+  shinyApp(ui = movies_ui(...),
     server = movies_server,
-    options = options
-  )
+    options = options)
 }

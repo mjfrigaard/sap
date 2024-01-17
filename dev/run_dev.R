656dcfe57a1ed8b980947203653e213527de5a07
Martin Frigaard <mjfrigaard@pm.me>
2024-01-17 14:47:58 -0700

    updates to documentation

---
+++ dev/run_dev.R
@@ -0,0 +1,16 @@
+# Set options here
+# TRUE = production mode, FALSE = development mode
+options(golem.app.prod = FALSE)
+
+# Comment this if you don't want the app to be served on a random port
+options(shiny.port = httpuv::randomPort())
+
+# Detach all loaded packages and clean your environment
+golem::detach_all_attached()
+# rm(list=ls(all.names = TRUE))
+
+# Document and reload your package
+golem::document_and_reload()
+
+# Run the application
+run_app()

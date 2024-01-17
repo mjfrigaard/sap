7c4a976605cc248b8e6aeee7527ec029c1c4d292
Martin Frigaard <mjfrigaard@pm.me>
2024-01-17 14:16:47 -0700

    updates to documentation

--- tests/testthat/fixtures/make-tidy_ggp2_movies.R
+++ tests/testthat/fixtures/make-tidy_ggp2_movies.R
@@ -1,40 +1,50 @@
-pkgs <- c("ggplot2movies", "tidyr", "dplyr", "stringr", "purrr")
-install.packages(pkgs, quiet = TRUE)
-library(tidyr)
-library(dplyr)
-library(stringr)
-library(purrr)
-
-tidy_ggp2_movies <- ggplot2movies::movies |>
-  tibble::add_column(row_id = 1:nrow(ggplot2movies::movies)) |>
-  tidyr::pivot_longer(c(Action:Short),
-    names_to = "genre_key",
-    values_to = "genre_value"
-  ) |>
-  dplyr::mutate(genre_value = as.logical(genre_value)) |>
-  dplyr::group_by(row_id) |>
-  dplyr::mutate(
-    genres = paste0(genre_key, collapse = ", ")
-  ) |>
-  dplyr::ungroup() |>
-  dplyr::distinct(title, .keep_all = TRUE) |>
-  dplyr::mutate(
-    genres = dplyr::na_if(x = genres, ""),
-    genres = factor(genres),
-    mpaa = dplyr::na_if(x = mpaa, y = ""),
-    mpaa = factor(mpaa,
-      levels = c("G", "PG", "PG-13", "R", "NC-17"),
-      labels = c("G", "PG", "PG-13", "R", "NC-17")
-    )
-  ) |>
-  dplyr::select(
-    title, genres, length, year,
-    budget, rating, votes, mpaa
+# tidy_movies
+make_tidy_ggp2_movies <- function(movies_data_url) {
+  movies_data <- read.csv(file = movies_data_url)
+  # specify genre columns
+  genre_cols <- c(
+    "Action", "Animation",
+    "Comedy", "Drama",
+    "Documentary", "Romance",
+    "Short"
   )
-# save to tests/testthat/fixtures/
-saveRDS(object = tidy_ggp2_movies, file = "tests/testthat/fixtures/tidy_ggp2_movies.rds")
+  # calculate row sum for genres
+  movies_data$genre_count <- rowSums(movies_data[, genre_cols])
+  # create aggregate 'genres' for multiple categories
+  movies_data$genres <- apply(
+    X = movies_data[, genre_cols],
+    MARGIN = 1,
+    FUN = function(row) {
+      genres <- names(row[row == 1])
+      if (length(genres) > 0) {
+        return(paste(genres, collapse = ", "))
+      } else {
+        return(NA)
+      }
+    }
+  )
+  # format variables
+  movies_data$genre_count <- as.integer(movies_data$genre_count)
+  movies_data$genre <- ifelse(test = movies_data$genre_count > 1,
+    yes = "Multiple genres",
+    no = movies_data$genres
+  )
+  movies_data$genre <- as.factor(movies_data$genre)
+  movies_data$mpaa <- factor(movies_data$mpaa,
+    levels = c("G", "PG", "PG-13", "R", "NC-17"),
+    labels = c("G", "PG", "PG-13", "R", "NC-17")
+  )
 
-# dist_titles <- unique(ggplot2movies::movies$title)
-# dist_tidy_titles <- unique(tidy_ggp2_movies$title)
-# waldo::compare(dist_titles, dist_tidy_titles)
-# identical(dist_titles, dist_tidy_titles)
+  # reduce columns to only those in graph
+  movies_data[, c(
+    "title", "year", "length", "budget",
+    "rating", "votes", "mpaa", "genre_count",
+    "genres", "genre"
+  )]
+}
+
+tidy_movies <- make_tidy_ggp2_movies("https://raw.githubusercontent.com/hadley/ggplot2movies/master/data-raw/movies.csv")
+
+# save to tests/testthat/fixtures/
+saveRDS(object = tidy_ggp2_movies, 
+  file = "tests/testthat/fixtures/tidy_ggp2_movies.rds")

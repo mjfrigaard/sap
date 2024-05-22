05004195a3839ee7a67617df6acde7a760d20635
mjfrigaard <mjfrigaard@users.noreply.github.com>
2024-05-22 20:35:44 +0000

    Style code (GHA)

--- R/movies_ui.R
+++ R/movies_ui.R
@@ -45,36 +45,35 @@ movies_ui <- function(bslib = FALSE) {
   )
   if (isFALSE(bslib)) {
     tagList(
-        bslib::page_fillable(
-          h1("Movie Reviews"),
-          bslib::layout_sidebar(
-            sidebar =
-              bslib::sidebar(
-                title = tags$h4("Sidebar inputs"),
-                img(
-                  src = "www/shiny.png",
-                  height = 60,
-                  width = 55,
-                  style = "margin:10px 10px"
-                ),
-                mod_var_input_ui("vars")
+      bslib::page_fillable(
+        h1("Movie Reviews"),
+        bslib::layout_sidebar(
+          sidebar =
+            bslib::sidebar(
+              title = tags$h4("Sidebar inputs"),
+              img(
+                src = "www/shiny.png",
+                height = 60,
+                width = 55,
+                style = "margin:10px 10px"
               ),
-            bslib::card(
-              full_screen = TRUE,
-              bslib::card_header(
-                tags$h4("Scatter Plot")
-              ),
-              mod_scatter_display_ui("plot"),
-              bslib::card_footer(
-                tags$blockquote(
-                  tags$em(
-                    tags$p(
-                      "The data for this application comes from the ",
-                      tags$a("Building web applications with Shiny",
-                        href = "https://rstudio-education.github.io/shiny-course/"
-                      ),
-                      "tutorial"
-                    )
+              mod_var_input_ui("vars")
+            ),
+          bslib::card(
+            full_screen = TRUE,
+            bslib::card_header(
+              tags$h4("Scatter Plot")
+            ),
+            mod_scatter_display_ui("plot"),
+            bslib::card_footer(
+              tags$blockquote(
+                tags$em(
+                  tags$p(
+                    "The data for this application comes from the ",
+                    tags$a("Building web applications with Shiny",
+                      href = "https://rstudio-education.github.io/shiny-course/"
+                    ),
+                    "tutorial"
                   )
                 )
               )
@@ -82,6 +81,7 @@ movies_ui <- function(bslib = FALSE) {
           )
         )
       )
+    )
   } else {
     tagList(
       bslib::page_fillable(

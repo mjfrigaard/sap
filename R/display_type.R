8e30d1ee9de9be7e8c2804b3eff65c5ff7aabac7
Martin Frigaard <mjfrigaard@pm.me>
2024-07-22 07:29:14 -0700

    updates to launch_app and display_type

--- R/display_type.R
+++ R/display_type.R
@@ -4,6 +4,11 @@
 #'  * `"p"` = launch in viewer pane 
 #'  * `"b"` = launch in external browser  
 #'  * `"w"` = launch in window (default)
+#' 
+#' @section IDE detection: 
+#' The function will check if the application is being run in RStudio 
+#' (with `Sys.getenv()`) and only apply the `shiny.launch.browser` 
+#' option if `TRUE`.
 #'
 #' @return notification of `shinyViewerType` option
 #' 
@@ -11,23 +16,22 @@
 #'
 display_type <- function(run = "w") {
   if (Sys.getenv("RSTUDIO") == "1") {
+    switch(run,
+      p = options(shiny.launch.browser = .rs.invokeShinyPaneViewer),
+      b = options(shiny.launch.browser = .rs.invokeShinyWindowExternal),
+      w = options(shiny.launch.browser = .rs.invokeShinyWindowViewer),
+      NULL = options(shiny.launch.browser = NULL))
+    
     environment <- "RStudio"
-  } else if (Sys.getenv("TERM_PROGRAM") == "vscode") {
-    environment <- "VS Code"
+    shinyViewerType <- getOption('shiny.launch.browser') |> 
+                        attributes() |> 
+                        unlist() |> 
+                        unname()
+                      
+    cli::cli_alert_info("Running app in {environment}")
+    cli::cli_alert_info("shinyViewerType set to {shinyViewerType}")
   } else {
-    environment <- "R"
+    environment <- "RStudio"
+    cli::cli_alert_info("App is not running in {environment}")
   }
-  
-  switch(run,
-    p = options(shiny.launch.browser = .rs.invokeShinyPaneViewer),
-    b = options(shiny.launch.browser = .rs.invokeShinyWindowExternal),
-    w = options(shiny.launch.browser = .rs.invokeShinyWindowViewer),
-    NULL = options(shiny.launch.browser = NULL))
-  
-  shinyViewerType <- getOption('shiny.launch.browser') |> 
-                      attributes() |> 
-                      unlist() |> 
-                      unname()
-  cli::cli_alert_info("Running in {environment}")
-  cli::cli_alert_info("shinyViewerType set to {shinyViewerType}")
 }

f717c0baf6fd14656a27a15cb109e4665b2dc316
Martin Frigaard <mjfrigaard@pm.me>
2024-07-23 13:30:35 -0700

    change display_type() (again)

--- tests/testthat/test-scatter_plot.R
+++ tests/testthat/test-scatter_plot.R
@@ -1,16 +1,16 @@
-testthat::describe(
+describe(
   "Feature: Scatter plot data visualization
        As a film data analyst
        I want to explore movie review data from IMDB.com
        So that I can analyze relationships between movie reivew metrics",
   code = {
-    testthat::describe(
+    describe(
       "Background:
        Given I have data with IMDB movie reviews
        And the data contains continuous variables like 'rating'
        And the data contains categorical variables like 'mpaa'",
       code = {
-        testthat::it(
+        it(
           "Scenario: Create scatter plot
              Given I have launched the movie review exploration app,
              When the scatter plot renders,

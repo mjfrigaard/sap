87ddd8f1d12469c257fa42fd1ab5fc9d307cc775
mjfrigaard <mjfrigaard@users.noreply.github.com>
2024-07-30 06:36:55 +0000

    Style code (GHA)

--- R/display_type.R
+++ R/display_type.R
@@ -1,12 +1,12 @@
 #' Shiny app display mode helper
 #'
-#' @param run where to launch app: 
-#'  * `"p"` = launch in viewer pane 
-#'  * `"b"` = launch in external browser  
+#' @param run where to launch app:
+#'  * `"p"` = launch in viewer pane
+#'  * `"b"` = launch in external browser
 #'  * `"w"` = launch in window (default)
 #'
 #' @return notification of `shinyViewerType` option
-#' 
+#'
 #' @export
 #'
 display_type <- function(run = "w") {
@@ -15,17 +15,18 @@ display_type <- function(run = "w") {
       p = options(shiny.launch.browser = .rs.invokeShinyPaneViewer),
       b = options(shiny.launch.browser = .rs.invokeShinyWindowExternal),
       w = options(shiny.launch.browser = .rs.invokeShinyWindowViewer),
-      NULL = options(shiny.launch.browser = NULL))
+      NULL = options(shiny.launch.browser = NULL)
+    )
     environment <- "RStudio"
-    shinyViewerType <- getOption('shiny.launch.browser') |> 
-                        attributes() |> 
-                        unlist() |> 
-                        unname()
-                      
+    shinyViewerType <- getOption("shiny.launch.browser") |>
+      attributes() |>
+      unlist() |>
+      unname()
+
     cli::cli_alert_info("App running in {environment}")
     cli::cli_alert_info("shinyViewerType set to {shinyViewerType}")
   } else {
     environment <- "RStudio"
     cli::cli_alert_info("App not running in {environment}")
-  } 
+  }
 }

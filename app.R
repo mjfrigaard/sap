85059f8e0d5d450683fe4818ab0b1a3bc0f426a5
Martin Frigaard <mjfrigaard@pm.me>
2024-08-01 08:16:53 -0700

    change name to shinyrPkgs

--- app.R
+++ app.R
@@ -4,17 +4,17 @@ withr::with_options(new = list(shiny.autoload.r = FALSE), code = {
     sink(stderr(), type = "output")
     tryCatch(
       expr = {
-        library(shinyAppPkgs)
+        library(shinyrPkgs)
       },
       error = function(e) {
         pkgload::load_all()
       }
     )
     shinyAppDir(appDir = 
-                system.file("prod/app", package = "shinyAppPkgs"))
+                system.file("prod/app", package = "shinyrPkgs"))
   } else {
     pkgload::load_all()
   }
-  shinyAppPkgs::launch_app(options = list(test.mode = TRUE), 
+  shinyrPkgs::launch_app(options = list(test.mode = TRUE), 
     run = 'p')
 })

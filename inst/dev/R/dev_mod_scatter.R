d0640850ab371c315f13f8f38aeae7b4d78e8d2b
Martin Frigaard <mjfrigaard@pm.me>
2024-09-03 22:42:57 -0700

    change shinyrPkgs > sap

--- inst/dev/R/dev_mod_scatter.R
+++ inst/dev/R/dev_mod_scatter.R
@@ -55,7 +55,7 @@ dev_mod_scatter_server <- function(id, var_inputs) {
 
     observe({
         output$scatterplot <- renderPlot({
-          plot <- shinyrPkgs::scatter_plot(
+          plot <- sap::scatter_plot(
             df = graph_data(),
             x_var = inputs()$x,
             y_var = inputs()$y,
--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
-library(shinyrPkgs)
+library(sap)
 
-test_check("shinyrPkgs")
+test_check("sap")

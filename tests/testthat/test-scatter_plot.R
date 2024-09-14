5daa30ad13f428d1a5a6f514cfb9ac6ec2e6aa9f
Martin Frigaard <mjfrigaard@pm.me>
2024-09-14 07:04:39 -0700

    updates to test

--- tests/testthat/test-scatter_plot.R
+++ tests/testthat/test-scatter_plot.R
@@ -1,46 +1,38 @@
 describe(
   "Feature: Scatter plot data visualization
-       As a film data analyst
-       I want to explore movie review data from IMDB.com
-       So that I can analyze relationships between movie reivew metrics",
-  code = {
-    describe(
-      "Background:
-          Given I have data with IMDB movie reviews
-          And the data contains continuous variables like 'rating'
-          And the data contains categorical variables like 'mpaa'",
-      code = {
-        it(
-          "Scenario: Create scatter plot
-              Given I have launched the movie review exploration app,
-              When the scatter plot renders,
-              Then the points on the x axis should represent 'Ratings'
-              And the points on the y axis should represent 'Length'
-              And the points should be colored by 'MPAA' rating
-              And the size of the points should be set to '2'
-              And the opacity of the points should be set to '0.5'",
-          code = {
-            # inputs
-            ggp2_scatter_inputs <- list(x = "rating",
-              y = "length",
-              z = "mpaa",
-              alpha = 0.75,
-              size = 3,
-              plot_title = "Enter plot title")
-            # data fixture
-            ggp2_movies <- readRDS(test_path(
-              "fixtures",
-              "tidy_ggp2_movies.rds"
-            ))
-
-            app_graph <- scatter_plot(ggp2_movies,
-              x_var = ggp2_scatter_inputs$x,
-              y_var = ggp2_scatter_inputs$y,
-              col_var = ggp2_scatter_inputs$z,
-              alpha_var = ggp2_scatter_inputs$alpha,
-              size_var = ggp2_scatter_inputs$size
-            )
-            expect_true(ggplot2::is.ggplot(app_graph))
-        })
-    })
+     As a film data analyst
+     I want to explore IMDB.com movie review data 
+     So that I can examine relationships between movie reivew metrics
+   Background:
+     Given I have IMDB movie reviews data  
+     And the data contains continuous variables like 'rating'
+     And the data contains categorical variables like 'mpaa'", 
+     code = {
+    it(
+      "Scenario: Scatter plot initial x, y, color values 
+         When I launch the Scatter Plot Data Visualization
+         And I have a IMDB dataset of movie reviews 
+         Then the plot should show 'Rating' on the x-axis
+         And the plot should show 'Length' on the y-axis
+         And the points on the plot should be colored by 'MPAA' rating", 
+        code = {
+    ggp2_scatter_inputs <- list(
+          x = "rating",
+          y = "length",
+          z = "mpaa",
+          alpha = 0.75,
+          size = 3,
+          plot_title = "Enter plot title"
+        )
+    tidy_ggp2_movies <- readRDS(test_path("fixtures",
+                                "tidy_ggp2_movies.rds"))
+    app_graph <- scatter_plot(tidy_ggp2_movies,
+      x_var = ggp2_scatter_inputs$x,
+      y_var = ggp2_scatter_inputs$y,
+      col_var = ggp2_scatter_inputs$z,
+      alpha_var = ggp2_scatter_inputs$alpha,
+      size_var = ggp2_scatter_inputs$size
+    )
+    expect_true(ggplot2::is.ggplot(app_graph))
+  }) 
 })

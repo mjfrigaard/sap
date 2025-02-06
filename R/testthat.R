4ed7bc86b5e14ee1481c5503a6722619f7ee5a66
Martin Frigaard <mjfrigaard@pm.me>
2025-02-06 07:53:40 -0700

    updates to test_logger

--- R/testthat.R
+++ R/testthat.R
@@ -1,6 +1,6 @@
 #' Log test messages (helper)
 #'
-#' `test_logger()` logs informational messages using the `logger` package. It 
+#' `test_logger()` logs informational messages using the `cli` package. It 
 #' allows optional `start` and `end` tags to indicate the beginning and end of
 #' a process.
 #'
@@ -11,10 +11,10 @@
 #' @details
 #' - If both `start` and `end` are `NULL`, logs only the message.
 #' - If `start` is provided and `end` is `NULL`, logs a message prefixed with 
-#' `[ START <start> = <msg> ]`.
+#' `START <start> = <msg>`.
 #' - If `start` is `NULL` and `end` is provided, logs a message prefixed with 
-#' `[ END <end> = <msg> ]`.
-#' - If both `start` and `end` are provided, logs both `[ START ]` and `[ END ]`
+#' `END <end> = <msg>`.
+#' - If both `start` and `end` are provided, logs both `START` and `END`
 #'  messages.
 #'
 #' @return Logs messages and returns `NULL` (used for side effects).
@@ -31,17 +31,17 @@
 test_logger <- function(start = NULL, end = NULL, msg) {
   if (is.null(start) & is.null(end)) {
     cat("\n")
-    logger::log_info("{msg}")
+    cli::cli_inform("TEST:[{format(Sys.time(), '%Y-%m-%d %H:%M:%S')}] = {msg}")
   } else if (!is.null(start) & is.null(end)) {
     cat("\n")
-    logger::log_info("\n[ START {start} = {msg}]")
+    cli::cli_inform("\nTEST: START [{format(Sys.time(), '%Y-%m-%d %H:%M:%S')}] {start} = {msg}")
   } else if (is.null(start) & !is.null(end)) {
     cat("\n")
-    logger::log_info("\n[ END {end} = {msg}]")
+    cli::cli_inform("\nTEST: END [{format(Sys.time(), '%Y-%m-%d %H:%M:%S')}] {end} = {msg}")
   } else {
     cat("\n")
-    logger::log_info("\n[ START {start} = {msg}]")
+    cli::cli_inform("\nTEST: START [{format(Sys.time(), '%Y-%m-%d %H:%M:%S')}]{start} = {msg}")
     cat("\n")
-    logger::log_info("\n[ END {end} = {msg}]")
+    cli::cli_inform("\nTEST: END [{format(Sys.time(), '%Y-%m-%d %H:%M:%S')}] {end} = {msg}")
   }
 }

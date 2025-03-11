78fc5011a4969bd54ddd9f062223cdc90a4268b7
Martin Frigaard <mjfrigaard@pm.me>
2025-03-10 22:20:44 -0700

    updates to workflow for docker

--- deploy.R
+++ deploy.R
@@ -1,7 +1,10 @@
-setAccountInfo(name = Sys.getenv("RSCONNECT_USER"),
+library(rsconnect)
+# set info
+rsconnect::setAccountInfo(name = Sys.getenv("RSCONNECT_USER"),
                token = Sys.getenv("RSCONNECT_TOKEN"),
                secret = Sys.getenv("RSCONNECT_SECRET"))
-deployApp(appDir = ".", 
+# deploy
+rsconnect::deployApp(appDir = ".", 
   appName = "shinyAppPkgsDockerCiCd", 
   account = "mjfrigaard", 
     server = "shinyapps.io", 

34f96ec429f113b2bc6d3c7c553595a548eaa711
Martin Frigaard <mjfrigaard@pm.me>
2025-06-11 22:15:57 -0700

    updates to .Rprofile

--- .Rprofile.R
+++ .Rprofile.R
@@ -1,7 +1,40 @@
+# usethis package ----
 if (interactive()) {
   require(usethis, quietly = TRUE)
 }
-
+# pak package ----
+if (interactive()) {
+  require(pak, quietly = TRUE)
+}
+# ellmer llm package ----
+if (interactive()) {
+  require(ellmer, quietly = TRUE)
+}
+# btw llm package ----
+if (interactive()) {
+  require(btw, quietly = TRUE)
+}
+options(
+  ## btw config ----
+   btw.chat_client = ellmer::chat_anthropic(),
+  # printing configs ----
+  width = 50L,
+  pillar.width = 50L,
+  width = 80,
+  str = utils::strOptions(strict.width = 'cut'),
+# description configs ----
+  usethis.description = list(
+    "Authors@R" = utils::person(
+        "Martin", "Frigaard",
+        email = "mjfrigaard@pm.me",
+        role = c("aut", "cre")
+    )
+  ),
+# repos configs ----
+    repos = c(pm = "https://packagemanager.posit.co/cran/latest",
+            CRAN = "https://cloud.r-project.org")
+)
+### clean_vignettes ----
 clean_vignettes <- function(pth = "vignettes/") {
   # all HTML files/folders ending with "_files"
   files_to_delete <- list.files(
@@ -19,7 +52,6 @@ clean_vignettes <- function(pth = "vignettes/") {
   unlink(files_to_delete, recursive = TRUE)
   # return
   return(invisible())
-  
 }
 # run 
 if (interactive()) {

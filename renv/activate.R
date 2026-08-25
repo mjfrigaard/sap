8dab89381cab97d1cf6b637869f442e06192d572
Martin Frigaard <mjfrigaard@pm.me>
2026-08-25 09:57:05 -0700

    updates to renv

--- renv/activate.R
+++ renv/activate.R
@@ -2,8 +2,8 @@
 local({
 
   # the requested version of renv
-  version <- "1.2.1"
-  attr(version, "md5") <- "51fff37c14949274ac148a9a087ab6bd"
+  version <- "1.2.4"
+  attr(version, "md5") <- "d5a3bd382107712f897630627794f81a"
   attr(version, "sha") <- NULL
 
   # the project directory

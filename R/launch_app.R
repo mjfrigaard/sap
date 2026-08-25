8556b0f87806edffe9566288ccc04907f143968d
Martin Frigaard <mjfrigaard@pm.me>
2026-08-25 12:25:01 -0700

    changes to launch_app(): add mode/startApp() dispatch

--- R/launch_app.R
+++ R/launch_app.R
@@ -2,18 +2,21 @@
 #'
 #' Starts the Movies Review Shiny application, which provides a customizable
 #' scatter plot interface for analyzing movie data.
-#' 
+#'
 #' @param app which app to run. Options are:
-#'  * `"movies"` = Default app  
-#'  * `"bslib"` = Alternative `bslib` layout 
+#'  * `"movies"` = Default app
+#'  * `"bslib"` = Alternative `bslib` layout
 #'  * `"ggp2"` = `ggplot2movies` (tidy) data app.
 #'  * `"quarto"` = Quarto movies app.
 #' @param options arguments to pass to `options()`
+#' @param mode launch mode. Choices are:
+#'  * `run` = blocking, uses [display_type()] for window placement (default)
+#'  * `start` = non-blocking, launches in a background process
 #' @param run where to launch app:
 #'  * `p` = launch in viewer pane
 #'  * `b` = launch in external browser
 #'  * `w` = launch in window
-#' @param ... arguments passed to UI 
+#' @param ... arguments passed to UI
 #'
 #' @return A **Shiny application** object.
 #'
@@ -27,25 +30,32 @@
 #' - [`movies_server()`] for the server logic.
 #'
 #' @family **Standalone Application**
-#' 
+#'
 #' @details
 #' See the [ggplot2movies](https://github.com/hadley/ggplot2movies) package.
-#' 
+#'
 #'
 #' @examples
 #' if (interactive()) {
 #'   launch_app()
 #' }
-#' 
+#'
 #' @export
-#' 
+#'
 #' @import shiny
-#' 
-launch_app <- function(app = "movies", options = list(), run = "p", ...) {
-  if (interactive()) {
-    display_type(run = run)
+#'
+launch_app <- function(app = "movies", options = list(), mode = "run", run = "p", ...) {
+
+  if (app == "quarto") {
+    return(
+      quarto::quarto_preview(
+        system.file("quarto", "index.qmd",
+            package = "sap" ),
+        render = "all")
+    )
   }
-  if (app == "bslib") {
+
+  app_obj <- if (app == "bslib") {
     shinyApp(
       ui = movies_ui(bslib = TRUE),
       server = movies_server,
@@ -58,16 +68,20 @@ launch_app <- function(app = "movies", options = list(), run = "p", ...) {
       ),
       options = options
       )
-  } else if (app == "quarto") {
-      quarto::quarto_preview(
-        system.file("quarto", "index.qmd",
-            package = "sap" ), 
-        render = "all")
   } else {
     shinyApp(
       ui = movies_ui(...),
       server = movies_server,
       options = options
     )
+  }
+
+  if (mode == "start") {
+    shiny::startApp(appDir = app_obj, port = 3838)
+  } else {
+    if (interactive()) {
+      display_type(run = run)
+    }
+    shiny::runApp(appDir = app_obj)
   }
 }
